2fe055c0323bbaaad5bdfae3f50933b4c038aeba
Nishu-28 <nishu28n18v7d@gmail.com>
2026-02-27 10:26:48 +0530

    UC7-fixed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -13,24 +13,19 @@ import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        // Define input string
         String input = "refer";
 
-        // Create Deque
         Deque<Character> deque = new LinkedList<>();
 
-        // Insert characters into deque
         for (char c : input.toCharArray()) {
-            deque.addLast(c);   // insert at rear
+            deque.addLast(c);
         }
 
-        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        // Compare front and rear elements
         while (deque.size() > 1) {
-            char front = deque.removeFirst(); // remove from front
-            char rear = deque.removeLast();   // remove from rear
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
 
             if (front != rear) {
                 isPalindrome = false;
@@ -38,7 +33,6 @@ public class PalindromeCheckerApp {
             }
         }
 
-        // Display result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
