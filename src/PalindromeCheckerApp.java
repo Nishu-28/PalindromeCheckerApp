a5a44c3d28a9e648214f36e3d26b4a2c8f6f3b22
Nishu-28 <nishu28n18v7d@gmail.com>
2026-03-09 08:54:25 +0530

    UC10

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -11,75 +11,22 @@
 import java.util.*;
 
 public class PalindromeCheckerApp {
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
-        String input = "level";
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        Node head = null, tail = null;
-        for (char c : input.toCharArray()) {
-            Node newNode = new Node(c);
-            if (head == null) {
-                head = tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
             }
         }
-
-        boolean isPalindrome = isPalindrome(head);
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-    }
-
-    public static boolean isPalindrome(Node head) {
-
-        if (head == null || head.next == null) return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        Node temp = secondHalf;
-        while (temp != null) {
-            if (firstHalf.data != temp.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            temp = temp.next;
-        }
-
-        return true;
-    }
-
-    public static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-
-        while (current != null) {
-            Node next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev;
     }
 }
